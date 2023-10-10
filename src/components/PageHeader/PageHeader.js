9f75d3dea671946708b1e44e794cd37c1c6d8de0
Fateme <fa.bagherzad@gmail.com>
2023-10-10 15:12:02 -0400

    create Front End of Inventory table, header and page header and style it

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,18 @@
+import Button from '../Button/Button';
+import InputAllTextType from '../InputAllTextType/InputAllTextType';
+import './PageHeader.scss';
+
+const PageHeader = ({ pageTitle, btnTxt, type }) => {
+  return (
+    <>
+      <header className="PageHeader container">
+        <h1 className="PageHeader__left">{pageTitle}</h1>
+        <div className="PageHeader__right">
+          <InputAllTextType type={type} />
+          <Button btnTxt={btnTxt} />
+        </div>
+      </header>
+    </>
+  );
+};
+export default PageHeader;
