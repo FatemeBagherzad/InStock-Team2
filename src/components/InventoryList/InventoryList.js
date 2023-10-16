8737ecdad634380196855fde676381e4001d4037
Fateme <fa.bagherzad@gmail.com>
2023-10-16 03:55:54 -0400

    filter showing inventorie for none zero quatity inventory

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,11 +3,14 @@ import InventoryItemTabDes from '../InventoryItemTabDes/InventoryItemTabDes';
 import InventoryListHeader from '../InventoryListHeader/InventoryListHeader';
 
 const InventoryList = ({ allInvetories, handleDeleteClick }) => {
+  const nonZeroInventories = allInvetories.filter(
+    (inventory) => inventory.quantity > 0
+  );
   return (
     <>
       <InventoryListHeader />
       <section>
-        {allInvetories.map((inventory) => (
+        {nonZeroInventories.map((inventory) => (
           <InventoryItemMob
             inventory={inventory}
             key={inventory.id}
@@ -16,7 +19,7 @@ const InventoryList = ({ allInvetories, handleDeleteClick }) => {
         ))}
       </section>
       <section>
-        {allInvetories.map((inventory) => (
+        {nonZeroInventories.map((inventory) => (
           <InventoryItemTabDes
             inventory={inventory}
             key={inventory.id}
