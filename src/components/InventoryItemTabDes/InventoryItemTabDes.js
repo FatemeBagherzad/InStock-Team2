4b5c62e6e208fc0a551451ae57920e4f3eb56a67
Fateme <fa.bagherzad@gmail.com>
2023-10-16 00:45:32 -0400

    fixing warehouse column in inventory page

--- src/components/InventoryItemTabDes/InventoryItemTabDes.js
+++ src/components/InventoryItemTabDes/InventoryItemTabDes.js
@@ -3,67 +3,84 @@ import './InventoryItemTabDes.scss';
 import arrowIcon from '../../assets/Icons/chevron_right-24px.svg';
 import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
 import editIcon from '../../assets/Icons/edit-24px.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const InventoryItemTabDes = ({ inventory, handleDeleteClick }) => {
+  const [warehouse, setWarehouse] = useState(null);
   const navigate = useNavigate();
+  const warehouseid = inventory.warehouse_id;
+  console.log(warehouseid);
+  useEffect(() => {
+    if (warehouseid) {
+      axios
+        .get(`http://localhost:8888/warehouses/` + warehouseid)
+        .then((response) => {
+          setWarehouse(response.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [warehouseid]);
 
-  // // Function to handle the edit icon click
-  // const handleEditClick = () => {
-  //   navigate(`/inventory/edit/${inventory.id}`);
-  // };
-
+  if (warehouse === null) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <div className="container inventoryItemTabDesDisplay ">
-        <ul className="inventoryItemTabDes">
-          <li
-            className="inventoryItemTabDes__li inventoryItemTabDes__li--withIcn"
-            onClick={() => navigate(`/inventory/${inventory.id}`)}
-          >
-            {inventory.item_name}{' '}
-            <img
-              className="inventoryItemTabDes__li--arrowIcn "
-              src={arrowIcon}
-              alt="Right arrow icon"
-            />
-          </li>
-          <li className="inventoryItemTabDes__li">{inventory.category}</li>
-          <li className="inventoryItemTabDes__li">
-            <div
-              className={`inventoryItemTabDes__li--status  ${
-                inventory.quantity > 0
-                  ? 'inventoryItemTabDes__li--status-inStock'
-                  : 'inventoryItemTabDes__li--status-outOfStock'
-              }`}
+      {warehouseid && (
+        <div className="container inventoryItemTabDesDisplay ">
+          <ul className="inventoryItemTabDes">
+            <li
+              className="inventoryItemTabDes__li inventoryItemTabDes__li--withIcn"
+              onClick={() => navigate(`/inventory/${inventory.id}`)}
             >
-              {inventory.status}
-            </div>
-          </li>
-          <li className="inventoryItemTabDes__li">{inventory.quantity}</li>
-          <li className="inventoryItemTabDes__li">Washington</li>
-          <li className="inventoryItemTabDes__li">
-            <div className="inventoryItemTabDes__li--delAnEdIcn">
-              <Link to={''}>
-                <img
-                  className="inventoryItemTabDes__li--delAnEdIcn-icon"
-                  src={deleteIcon}
-                  alt="delete icon"
-                  onClick={() =>
-                    handleDeleteClick(true, inventory.id, inventory.item_name)
-                  }
-                />
-              </Link>
-              <Link to={`/inventory/edit/${inventory.id}`}>
-                <img
-                  className="inventoryItemTabDes__li--delAnEdIcn-icon"
-                  src={editIcon}
-                  alt="edit icon"
-                />
-              </Link>
-            </div>
-          </li>
-        </ul>
-      </div>
+              {inventory.item_name}{' '}
+              <img
+                className="inventoryItemTabDes__li--arrowIcn "
+                src={arrowIcon}
+                alt="Right arrow icon"
+              />
+            </li>
+            <li className="inventoryItemTabDes__li">{inventory.category}</li>
+            <li className="inventoryItemTabDes__li">
+              <div
+                className={`inventoryItemTabDes__li--status  ${
+                  inventory.quantity > 0
+                    ? 'inventoryItemTabDes__li--status-inStock'
+                    : 'inventoryItemTabDes__li--status-outOfStock'
+                }`}
+              >
+                {inventory.status}
+              </div>
+            </li>
+            <li className="inventoryItemTabDes__li">{inventory.quantity}</li>
+            <li className="inventoryItemTabDes__li">
+              {warehouse[0].warehouse_name}
+            </li>
+            <li className="inventoryItemTabDes__li">
+              <div className="inventoryItemTabDes__li--delAnEdIcn">
+                <Link to={''}>
+                  <img
+                    className="inventoryItemTabDes__li--delAnEdIcn-icon"
+                    src={deleteIcon}
+                    alt="delete icon"
+                    onClick={() =>
+                      handleDeleteClick(true, inventory.id, inventory.item_name)
+                    }
+                  />
+                </Link>
+                <Link to={`/inventory/edit/${inventory.id}`}>
+                  <img
+                    className="inventoryItemTabDes__li--delAnEdIcn-icon"
+                    src={editIcon}
+                    alt="edit icon"
+                  />
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
